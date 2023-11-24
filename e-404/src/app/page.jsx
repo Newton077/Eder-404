f9a2795ef50d4068fa4617ddbd9378bab4f0b793
Eder <eder077@proton.me>
2023-11-23 22:14:50 -0600

    pagina probicional en contruccion
    soon

---
+++ e-404/src/app/page.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import './globals.css'
+
+
+export default function Home() {
+  return (
+    <main className="bg-black">
+      <div className="">
+
+      </div>
+       
+      <div className="soon">
+        <h1>Eder-404</h1>
+        <p>En contruccion</p>
+
+        
+      </div>
+
+
+   
+    </main>
+    
+  );
+}
